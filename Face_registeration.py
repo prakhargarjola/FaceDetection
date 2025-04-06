f7d5e0bb74130b2b35270156a2b4a7ac9b5cbe6d
prakhargarjola <prakhargarjola05@gmail.com>
2025-04-06 21:16:02 +0530

    minor changes

--- Face_registeration.py
+++ Face_registeration.py
@@ -38,10 +38,11 @@ while True:
         embedding = face.embedding.reshape(1, -1)
 
         is_duplicate = False
-        for reg_face in registered_faces:
-            sim = cosine_similarity(embedding, reg_face)[0][0]
+        for name, reg_embedding in registered_faces:
+            sim = cosine_similarity(embedding, reg_embedding)[0][0]
             if sim > 0.6:
                 is_duplicate = True
+                recognized_name = name
                 break
 
         if not is_duplicate:
@@ -49,20 +50,19 @@ while True:
             cv2.putText(frame, "New face - Press 's'", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
             key = cv2.waitKey(1) & 0xFF
             if key == ord('s'):
-                image_path = f"registered_faces/face_{face_count}.jpg"
-                cv2.imwrite(image_path, cropped)
-                registered_faces.append(embedding)
+                name = input("Enter name for this face: ")
+                registered_faces.append((name, embedding))
                 with open(embeddings_file, "wb") as f:
                     pickle.dump(registered_faces, f)
                 face_count += 1
         else:
-            cv2.putText(frame, "Already registered", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+            cv2.putText(frame, f"{recognized_name} (Recognized)", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
     cv2.imshow("Face Registration", frame)
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
-
+print(registered_faces)
 video.release()
 cv2.destroyAllWindows()
--- dataset_Encoding.py
+++ dataset_Encoding.py
@@ -8,8 +8,7 @@ img_extention = ['.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff']
 
 img_file = [file for file in folder.iterdir() if file.suffix.lower() in img_extention]
 img_number = len(img_file)
-
-
+file_name = [file.stem for file in img_file]
 
 app = FaceAnalysis(name='buffalo_l')
 app.prepare(ctx_id=0)
@@ -18,13 +17,16 @@ registered_faces = []
 saved_faces = 'dataset/saved_faces.pkl'
 
 for f_count in range(1, img_number+1):
-    face_1 = f'dataset/face_{f_count}.jpg'
+    face_1 = f'dataset/{file_name[f_count -1]}.jpg'
     img = cv2.imread(face_1)
 
     faces = app.get(img)
     for face in faces:
-        embedding = face.embedding.reshape(1, -1)
-        registered_faces.append(embedding)
+           x1, y1, x2, y2 = face.bbox.astype(int)
+           cropped = img[y1:y2, x1:x2]
+           embedding = face.embedding.reshape(1, -1)
+           name = file_name[f_count -1]
+           registered_faces.append((name, embedding))
 
 with open(saved_faces, 'wb') as f:
             pickle.dump(registered_faces, f)
